40b98c95273dffebe8f5382904bc71aeb63d38a6
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-07-06 10:38:22 -0400

    add babel6 vs 7 boilerplate

--- packages/babel-plugin-boilerplate/src/index.js
+++ packages/babel-plugin-boilerplate/src/index.js
@@ -2,20 +2,24 @@
 // TODO: ???
 // TODO: PROFIT
 
-// import semver from 'semver'
-// import jsx from '@babel/plugin-syntax-jsx'
-// import jsx6 from 'babel-plugin-syntax-jsx'
+/* for babel version compatibility */
+import semver from 'semver'
+import jsx from '@babel/plugin-syntax-jsx'
+import jsx6 from 'babel-plugin-syntax-jsx'
 
 export default ({ types: t, template, version }) => {
-	return {
-		name: 'ast-transform', // not required
-		visitor: {
-			Identifier(path) {
-				path.node.name = path.node.name
-					.split('')
-					.reverse()
-					.join('');
-			}
-		}
-	};
-};
+  const babel6 = semver.satisfies(version, '^6.0.0') // for babel version compatibility
+
+  return {
+    name: 'ast-transform', // optional
+    inherits: babel6 ? jsx6 : jsx, // for babel version compatibility
+    visitor: {
+      Identifier(path) {
+        path.node.name = path.node.name
+          .split('')
+          .reverse()
+          .join('')
+      },
+    },
+  }
+}
