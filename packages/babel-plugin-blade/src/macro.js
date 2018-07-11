8242d7794760c4ad2812236f039c2768df9aa4d7
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-07-11 00:16:30 -0400

    add basic macro setup

--- packages/babel-plugin-blade/src/macro.js
+++ packages/babel-plugin-blade/src/macro.js
@@ -1,17 +1,21 @@
-const {createMacro} = require('babel-plugin-macros')
+const { createMacro, MacroError } = require('babel-plugin-macros');
 // const {getReplacement} = require('./helpers')
-const {handleCreateRazor} = require('./index')
+const { handleCreateRazor } = require('./index');
 
-module.exports = {
-  createQuery: createMacro(bladeMacros),
-  createFragment: createMacro(bladeMacros),
-}
+export default createMacro(bladeMacros);
 
-function bladeMacros({references, state, babel: {types: t}}) {
-  references.createQuery.forEach(referencePath => {
-    handleCreateRazor(referencePath, t)
-  })
-  references.createFragment.forEach(referencePath => {
-    handleCreateRazor(referencePath, t)
-  })
+function bladeMacros({ references, state, babel: { types: t } }) {
+	Object.keys(references).forEach(referenceKey => {
+		if (referenceKey === 'createQuery' || referenceKey === 'createFragment') {
+			references[referenceKey].forEach(referencePath => {
+				handleCreateRazor(referencePath, t);
+			});
+		} else throw new MacroError('invalid require?');
+	});
+	// references.createQuery.forEach(referencePath => {
+	//   handleCreateRazor(referencePath, t)
+	// })
+	// references.createFragment.forEach(referencePath => {
+	//   handleCreateRazor(referencePath, t)
+	// })
 }
